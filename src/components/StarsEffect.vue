96c87e70d5305893f30bd3ca3372eba7eabdb5c5
Luca Spennato <luca.spennato@gmail.com>
2022-11-02 17:09:08 +0100

    feat: :sparkles: stars-like-effect

---
+++ src/components/StarsEffect.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="stars-wrapper">
+
+    <div class="stars" v-for="(star, i) in 100" :key="i">
+      
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'StarsEffect',
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .stars-wrapper{
+    overflow: hidden;
+    position: fixed;
+    height: 100%;
+    width: 100%;
+
+    .stars{ 
+      width: 3px;
+      height: 3px;
+      z-index: 0;
+      padding: 0;
+      background-color: gray;
+      border-radius: 100%;
+      animation: starsAnimation linear infinite;
+      position: absolute;
+
+    }
+
+    @for $i from 1 through 100{
+        .stars{
+          &:nth-child(#{$i}){
+            left: percentage(random(95) / 100);
+            animation-duration: random(10) + 5 + s;
+            bottom: percentage(random(80) / 100);
+            opacity: 0;
+  
+            @keyframes starsAnimation {
+              0%{
+                opacity: 0;
+              }
+              5%{
+                opacity: 1;
+              }
+              100%{
+                transform: translatey(-300px);
+                // bottom: 100%;
+              }
+            }
+          }
+
+        }
+      }
+  }
+</style>
