e0caf0e5320b4e8b6f2a5b543bf99935efcc681e
Luca Spennato <luca.spennato@gmail.com>
2022-11-07 09:58:01 +0100

    install and import of vue-fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import 
+{ faCircleChevronLeft as arrowLeft, faCircleChevronRight as arrowRight, faCircleChevronUp as arrowUp } 
+from '@fortawesome/free-solid-svg-icons'
+import 
+{ 
+    faGithub as github,
+    faVuejs as vue, 
+    faHtml5 as html,
+    faCss3 as css,
+    faBootstrap as bootstrap,
+    faSass as sass,
+    faGit as git,
+    faJs as js,
+    faPhp as php,
+    faLaravel as laravel,
+    faLinkedin as linkedin,
+
+} 
+from '@fortawesome/free-brands-svg-icons'
+
+library.add(github, vue, html, css, bootstrap, sass, git, js, php, laravel, linkedin, arrowLeft, arrowRight, arrowUp)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
